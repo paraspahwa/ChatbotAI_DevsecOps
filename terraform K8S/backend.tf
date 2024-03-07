d717cebf10f396e680e4b9099b74de40c20e3167
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2024-03-08 00:31:35 +0530

    Update backend.tf

--- terraform K8S/backend.tf
+++ terraform K8S/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "paraspahwa-demo-cloud5555" # Replace with your actual S3 bucket name
+    bucket = "paraspahwa-demo-cloud5555" 
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
