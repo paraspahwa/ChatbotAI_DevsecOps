a5712a938e599875b8c2d35d67ef5dff0ce87916
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2024-03-08 00:59:25 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "paras-demo-584774" 
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
