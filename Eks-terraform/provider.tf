97851b3f3624848416e91428ada48ddf126a8bc0
Paras Pahwa <134779342+paraspahwa@users.noreply.github.com>
2024-03-08 00:56:42 +0530

    Update provider.tf

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -10,4 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
+  access_key = "AKIAZQ3DNY35BZ5V3GFE"
+  secret_key = "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"
 }
